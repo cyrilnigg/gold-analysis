8ffc4dd3dfd4336cb1b9065a371943c0a6fa2f62
cyrilnigg <cyrilnigg@gmail.com>
2015-08-16 20:59:13 -0500

    Added month over month analysis

--- analysis.R
+++ analysis.R
@@ -1,13 +1,10 @@
 library("forecast")
+library("MASS")
 
 #Set this to the appropriate directory
-setwd("~/r/gold-prices")
+setwd("~/r/gold-analysis")
 
-gold <- read.csv("/data/historical-gold-prices.csv")
-summary(gold)
-
-### Load gold data Doug created ###
-gold <- read.csv('~/r/gold-prices/data/doug_gold3.csv')
+gold <- read.csv("data/doug_gold3.csv")
 summary(gold)
 
 #####CLEAN THE DATA #####
@@ -19,14 +16,16 @@ summary(gold$Date)
 #Convert columns into numeric values
 indx <- sapply(gold, is.factor)
 gold[indx] <- lapply(gold[indx], function(x) as.numeric(gsub(",", "", as.character(x))))
+summary(gold)
 
 #plot gold price over time
 plot(gold$Date, gold$Gold, type="l")
 
-#create a time series
+?#create a time series
 ts.gold <- ts(gold[2:2])
 summary(ts.gold)
 plot(ts.gold)
+
 
 #Create a variable for gold next month and gold in 6 months
 #These will be used as dependant variables
@@ -45,18 +44,30 @@ GoldSixMonth <- gold$Gold[7:nrow(gold)]
 GoldSixMonth[(nrow(gold)-6):nrow(gold)] <- NA
 gold$GoldSixMonth <- GoldSixMonth
 
+#Look at perecent change month-to-month as a log
+plot(log(gold$GoldNextMonth/gold$Gold))
 
 ##### DIVIDE DATA INTO TRAINING / TEST ######
 
 #Training set will be all data before 2007 to see if we can predict latest crash
-gold.training <- subset(gold, as.Date(Date) <= '2007-01-01')
+gold.train <- subset(gold, as.Date(Date) <= '2007-01-01')
+gold.train <- subset(gold.train, as.Date(Date) > '1972-12-31')
+
 gold.test <- subset(gold, as.Date(Date) > '2007-01-01')
 
 
 ##### BUILD LINEAR MODEL #####
-lm.gold <- lm(GoldNextMonth ~ Gold + S.P + US.Dollar.Index + CPI, data=gold)
+lm.gold <- lm(GoldNextMonth ~ Gold + S.P + US.Dollar.Index + CPI, data=gold.train)
 summary(lm.gold)
-anova(lm.gold)
+plot(lm.gold)
+
+#Test polynomial terms
+lm.poly <- lm(GoldNextMonth ~ Gold*S.P*US.Dollar.Index*CPI, data=gold.train)
+summary(lm.poly)
+
+#Try backwards stepwise regression
+lm.reduce.backwards <- step(lm.poly, direction="both")
+
 
 #This is simply to delete the last row which doesn't have "next month's" data
 gold.test <- gold.test[1:(nrow(gold.test)-1),] 
